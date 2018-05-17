e6ed693404e2a1eae29c541bd198fd61e1aed8e0
bojko108 <bojko108@gmail.com>
2018-05-17 15:22:53 +0300

    fix bullets velocity problem

--- Assets/Scripts/Player/BulletScript.cs
+++ Assets/Scripts/Player/BulletScript.cs
@@ -6,11 +6,20 @@ public class BulletScript : MonoBehaviour
 {
     private int enemiesLayer;
     private PlayerShooting playerShooting;
+    private Rigidbody bulletBody;
 
     private void Start()
     {
+        this.bulletBody = this.GetComponent<Rigidbody>();
         this.enemiesLayer = LayerMask.NameToLayer(Resources.Layers.Enemies);
         this.playerShooting = GameObject.FindGameObjectWithTag(Resources.Tags.Player).GetComponentInChildren<PlayerShooting>();
+    }
+
+    private void OnDisable()
+    {
+        // after a collision, velocity parameters get changed...
+        this.bulletBody.velocity = Vector3.zero;
+        this.bulletBody.angularVelocity = Vector3.zero;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -18,7 +27,7 @@ public class BulletScript : MonoBehaviour
         this.gameObject.SetActive(false);
 
         GameObject hit = collision.gameObject;
-        
+
         if (hit.layer == this.enemiesLayer)
         {
             this.playerShooting.HitEnemy(hit, collision.contacts[0].point);
